536189683deb56b80af82efaac16b0924fee746e
alonisser <alonisser@gmail.com>
2020-11-19 12:27:47 +0200

    more pain in main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///db.db', echo=True)
 
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+engine = create_engine('sqlite:///db.db', echo=True)
+Session = sessionmaker(bind=engine)
+
 
 class President(Base):
     __tablename__ = "presidents"
@@ -16,4 +18,9 @@ class President(Base):
 Base.metadata.create_all(engine)
 
 
+def run():
+    pass
 
+
+if __name__ == "__main__":
+    run()
